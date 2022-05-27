460c179869c33cf1b8fbb415f2d36711ecd0bcad
Franco Diaz2 <francoagustindiaz7@gmail.com>
2022-05-26 23:09:55 -0300

    Actualización de la página

--- Clase-3/funciones.js
+++ Clase-3/funciones.js
@@ -1,4 +1,18 @@
 // Recitar oldmacdonald
+function creative(texto) {
+  // crea el texto
+  let sHeader = document.createTextNode("" + texto);
+  // crea un elemento span para el texto y lo "append" it.
+  let spanHeader = document.createElement("span");
+  spanHeader.appendChild(sHeader);
+  // crea un elemento "br".
+  let brHeader = document.createElement("br");
+  // Adquiere el id del div
+  let divHeader = document.getElementById("escribir");
+  // append el nuevo elemento y lo "append" it.
+  divHeader.appendChild(spanHeader);
+  divHeader.appendChild(brHeader);
+}
 
 function oldmacdonald() {
   const animales = "cow,pig,chicken";
@@ -22,6 +36,48 @@ function oldmacdonald() {
   }
 }
 
+function oldmacdonald2() {
+  const animal = "cow,pig,chicken".split(",");
+  const sonido = "moo,oink,bawk".split(",");
+  for (let i = 0; i != 3; i++) {
+    creative("Old MACDONALD had a farm");
+    creative("E-I-E-I-O");
+    creative("And on his farm he had a " + animal[i]);
+    creative("E-I-E-I-O");
+    creative("With a " + sonido[i], "" + sonido[i], "here");
+    creative("And a " + sonido[i], "" + sonido[i], "there");
+    creative("Here a " + sonido[i] + ", there a", sonido[i]);
+    creative("Everywhere a " + sonido[i], "" + sonido[i]);
+    creative("Old MacDonald had a farm");
+    creative("E-I-E-I-O");
+    creative("");
+  }
+}
+function oldmacdonald3() {
+  const animal = "cow,pig,chicken".split(",");
+  const sonido = "moo,oink,bawk".split(",");
+  const numbers = [45, 4, 9, 16, 25];
+  const cantos = [
+    "Old MACDONALD had a farm",
+    "E-I-E-I-O",
+    "And on his farm he had a " + animal[i],
+    "E-I-E-I-O",
+    "With a " + sonido[i] + "" + sonido[i] + "here",
+    "And a " + sonido[i] + "" + sonido[i] + "there",
+    "Here a " + sonido[i] + ", there a" + sonido[i],
+    "Everywhere a " + sonido[i] + "" + sonido[i],
+    "Old MacDonald had a farm",
+    "E-I-E-I-O",
+    "",
+  ];
+  let txt = "";
+  cantos.forEach(myFunction);
+  document.getElementById("demo").innerHTML = txt;
+
+  function myFunction(value, index, array) {
+    txt += value + "<br>";
+  }
+}
 // Comenta la temperatura
 
 function temperatura(valor) {
@@ -35,6 +91,16 @@ function temperatura(valor) {
       : "Error";
 
   return output;
+}
+
+function temperatura2(valor) {
+  return 0 >= valor
+    ? "El arte es morirse de frío"
+    : 16 >= valor
+    ? "Está fresco apra pasear en jaula"
+    : 16 < valor
+    ? "Maldición, va a ser un día hermoso"
+    : "Error";
 }
 
 // Señala el signo
@@ -63,4 +129,27 @@ function login(Perfil) {
       ? "Sin sesión"
       : "Error";
   return cuenta;
+}
+
+// Funciones html
+// element.addEventListener("click", funcion);
+
+function mostrar(target) {
+  document.getElementById(target).setAttribute("class", "visible");
+}
+
+function ocultar(target) {
+  document.getElementById(target).setAttribute("class", "hidden");
+}
+
+function reset(target) {
+  document.getElementById(target).innerHTML = "";
+}
+
+function escribir(donde) {
+  let txt = "";
+  for (let i = 0; i < 5; i++) {
+    txt += +i + "<br>";
+  }
+  document.getElementById(donde).innerHTML = txt;
 }
