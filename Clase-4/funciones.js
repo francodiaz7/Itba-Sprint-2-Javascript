67b41087b9092dabf3df46c06dc7082c36ddf282
Franco Diaz <francoagustindiaz7@gmail.com>
2022-05-30 11:10:58 -0300

    Clase-4 agregada

---
+++ Clase-4/funciones.js
@@ -0,0 +1,46 @@
+function ucFirst(str) {
+  let resultado = str.charAt(0).toUpperCase() + ejemplo.slice(1);
+  return resultado;
+}
+
+//console.log(ucFirst('ejemplodo'));
+
+function checkSpam(str) {
+  let resultado = str.toLowerCase();
+  return resultado.includes('viagra') || resultado.includes('xxx');
+}
+
+//console.log(checkSpam('xxx'))
+//console.log(checkSpam('Xxx'))
+//console.log(checkSpam('viagra'))
+//console.log(checkSpam('argentina xxx'))
+
+function truncate(str, maxlength) {
+  return str.length > maxlength ? str = str.slice(0,maxlength) + '...' : str;
+}
+
+//console.log(truncate('argentina', 2))
+
+function extractCurrencyValue(str) {
+  return +str.slice(1);
+}
+
+//console.log(extractCurrencyValue('$534'));
+let variable = '10';
+//console.log(220 + Number(variable));
+
+// Tarea
+
+let oracion = "hola,como,estas,otrapalabra";
+let separado = oracion.split(",");
+
+console.log(separado[0]);
+console.log(separado[1]);
+console.log(separado[2]);
+console.log(separado[3]);
+
+let resultado = "hola,como,estas,otrapalabra".split(",");
+console.log(resultado[0]);
+console.log(resultado[1]);
+console.log(resultado[2]);
+console.log(resultado[3]);
