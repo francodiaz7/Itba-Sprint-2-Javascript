e66c6ce6e1c7bbd6b3a2f5432fa6b64011e45312
Franco Diaz2 <francoagustindiaz7@gmail.com>
2022-05-31 02:19:05 -0300

    Agregado ejercicio

---
+++ Clase-5/1.js
@@ -0,0 +1,21 @@
+let styles = ["Jazz", "Blues"];
+
+//console.log(styles); //print
+
+styles.push("Rock-n-Roll");
+
+//console.log(styles); //print
+
+let medio = (styles.length - 1) / 2;
+
+styles[medio] = "Classics";
+
+//console.log(styles); //print
+
+styles.shift();
+
+//console.log(styles); //print
+
+styles.unshift("Rap", "Reggae");
+
+//console.log(styles); //print
