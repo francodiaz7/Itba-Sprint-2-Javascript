5c54095aaee9036e040139b9c4dda62908bfea68
Franco Diaz <francoagustindiaz7@gmail.com>
2022-06-02 09:04:47 -0300

    Clase-5 completada

--- Clase-5/ejercicios.js
+++ Clase-5/ejercicios.js
@@ -15,18 +15,20 @@ let arrayCorreosAdmitidos = [
 let arrayCorreosDescartados = [];
 
 function verificarCorreo(correo) {
-  for (let i = 0; i < 3; i++) {
-    correo.forEach(function (element, index, arreglo) {
-      if (element.includes("@")) {
-        arrayCorreosAdmitidos += correo.splice(index, index + 1);
+  for (et i = 0l;correo.length != 0; i++) {
+      if (correo[i].includes("@") && correo[i].indexOf('@') != 0) {
+        arrayCorreosAdmitidos += correo.splice([i], [i] +1);
+        i -= 1;
       } else {
-        arrayCorreosDescartados += correo.splice(index, index + 1);
+        arrayCorreosDescartados += correo.splice([i], [i] +1);
+        i -=1;
       }
-    });
+    };
   }
-}
 console.log("--- Array de correos admitidos: " + "longitud de array" + " ---");
 console.log(arrayCorreosAdmitidos);
 console.log();
 console.log("---Array de correos Descartados:" + "longitud de array" + " ---");
 console.log(arrayCorreosDescartados);
+console.log('correos pendientes');
+console.log(arrayCorreosPendientes);
